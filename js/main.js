540ee56dbb35a670949cd5f7a2eb3b30bcbbc3a3
Erin Goncer and Liam Ellis <liam@>
2016-04-11 09:48:31 -0700

    add businnes logic object dog constructor

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function Dog(name, breed, age, colors, gender, temperment){
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+  this.colors = colors;
+  this.gender = gender;
+  this.temperment = temperment;
+}
+
+Dog.neutered = true;
+Dog.adopted = true;
+
+
+// UI LOGIC ******************************************************
